29fcac23a00989af83d28354a1ada5057159b964
Katrina Barth <katrina.barth@duke.edu>
2019-02-14 19:10:32 -0500

    Updated Style

--- TSH_Reader.py
+++ TSH_Reader.py
@@ -3,15 +3,19 @@
 # Reads in patient data and detects presence of hyper- or hypo- thyroidism
 
 # Function to read in patient data file
+
+
 def Read_File():
     f = open('test_data.txt', 'r')
     data = f.read()
     f.close()
     return data
 
+
 def main():
     data = Read_File()
     print(data)
+
 
 if __name__ == "__main__":
     main()
